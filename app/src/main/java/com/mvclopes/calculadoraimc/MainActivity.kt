ae06e0b6ff53ee6cc220c2ebfffb3af1b3314e54
Matheus Lopes <matheus.lopes23@hotmail.com>
2022-11-03 20:09:49 -0300

    Adicionada lógica a calculadora IMC

--- app/src/main/java/com/mvclopes/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/mvclopes/calculadoraimc/MainActivity.kt
@@ -2,10 +2,38 @@ package com.mvclopes.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mvclopes.calculadoraimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.btnMakeCalc.setOnClickListener {
+            val pesoTxt = binding.edtPeso.text.toString()
+            val alturaTxt = binding.edtAltura.text.toString()
+            val peso = pesoTxt.toFloat()
+            val altura = alturaTxt.toFloat()
+            val imc = peso / (altura * altura)
+            if (imc < 16){
+                binding.txtResultImc.text = "Magreza grave"
+            } else if (imc < 17){
+                binding.txtResultImc.text = "Magreza moderada"
+            } else if (imc < 18.5){
+                binding.txtResultImc.text = "Magreza leve"
+            } else if (imc < 25){
+                binding.txtResultImc.text = "Saudável"
+            } else if (imc < 30){
+                binding.txtResultImc.text = "Sobrepeso"
+            } else if (imc < 35){
+                binding.txtResultImc.text = "Obesidade Grau I"
+            } else if (imc < 40){
+                binding.txtResultImc.text = "Obesidade Grau II (severa)"
+            } else {
+                binding.txtResultImc.text = "Obesidade Grau III (mórbida)"
+            }
+        }
     }
 }
